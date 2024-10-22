c86889c87beed28d88c564168cebf1f22bf06384
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2024-10-22 18:23:37 -0300

    finish with dashboard

--- src/components/Dashboard/SectionFour.jsx
+++ src/components/Dashboard/SectionFour.jsx
@@ -1,39 +1,58 @@
-// src/components/SectionFour.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './Sections.css';
+import axios from 'axios';
 
-const data = [
-  { name: 'Producto A', value: 400 },
-  { name: 'Producto B', value: 300 },
-  { name: 'Producto C', value: 300 },
-  { name: 'Producto D', value: 200 },
-];
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#0088FE', '#00C49F', '#567BF5', '#FF8042', "#786976", "#A78EF5", "#ef9af1", "#8fee56", "#d469a1","#07D3F2"];
+// const COLORS = ['#0088FE', '#00C49F', '#567BF5', '#FF8042', "#786976", "#A78EF5", "#ef9af1", "#8fee56", "#d469a1","#07D3F2"];
 
 const SectionFour = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchTopRecipesData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/v1/topRecipes');
+        
+        // Convertir 'resultado' a número
+        const formattedData = response.data.map(item => ({
+          ...item,
+          resultado: parseFloat(item.resultado), // Convertir resultado a número
+        }));
+
+        setData(formattedData);
+        console.log(formattedData); // Verifica que los datos están bien formateados
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTopRecipesData();
+  }, []);
+
   return (
     <div className="section-four">
       <h3>Distribución de Productos</h3>
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={({ name, percent }) => `${name} (${(percent * 100).toFixed(0)}%)`}
-            outerRadius={100}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
+          {data.length > 0 && (
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={({ NombreReceta, percent }) => `${NombreReceta} (${(percent * 100).toFixed(0)}%)`}
+              outerRadius={100}
+              fill="#8884d8"
+              dataKey="resultado"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          )}
           <Tooltip />
-          <Legend />
+          {/* <Legend /> */}
         </PieChart>
       </ResponsiveContainer>
     </div>
